3cba782b833c140248f7bb3a7803cc406e08a619
bedwardsphoto <brian@brianedwardsphotography.com>
2016-01-27 23:44:34 -0500

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,16 @@
+class Dog
+def initialize(name,breed="Mut")
+  @name=name
+  @breed=breed
+end
+def name=(name)
+end
+def name
+  @name
+end
+def breed=(breed="Mut")
+  end
+  def breed
+    @breed
+  end
+end
